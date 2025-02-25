027423bea9891fdc1daa328a0455b82fc05c61f4
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-25 18:10:29 -0500

    init inlang project

--- inlang/packages/paraglide/paraglide-js/examples/tanstack-router/vite.config.ts
+++ inlang/packages/paraglide/paraglide-js/examples/tanstack-router/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
 		TanStackRouterVite({
 			target: "react",
 			autoCodeSplitting: true,
+		}),
+		paraglideVitePlugin({
+			project: "./project.inlang",
+			outdir: "./src/paraglide",
+			strategy: ["url", "baseLocale"],
 		}),
 		react(),
 	],
