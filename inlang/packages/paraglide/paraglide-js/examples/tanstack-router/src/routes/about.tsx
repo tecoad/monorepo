b7cddfb26528466a4c1462fe45d0f56050ef654a
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2025-02-25 19:09:55 -0500

    init paraglide messages

--- inlang/packages/paraglide/paraglide-js/examples/tanstack-router/src/routes/about.tsx
+++ inlang/packages/paraglide/paraglide-js/examples/tanstack-router/src/routes/about.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
+import { m } from "../paraglide/messages";
 
 export const Route = createFileRoute('/about')({
   component: AboutComponent,
@@ -7,8 +8,8 @@ export const Route = createFileRoute('/about')({
 
 function AboutComponent() {
   return (
-    <div className="p-2">
-      <h3>About</h3>
-    </div>
-  )
+		<div className="p-2">
+			<h3>{m.about()}</h3>
+		</div>
+	);
 }
